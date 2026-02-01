a4d5212918b35af321b1780d63a98b6d1cb67ad6
GoonKatze <164936743+goonkatze@users.noreply.github.com>
2026-02-01 02:15:05 +0100

    Rename NoFallModule for clarity

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -6,7 +6,7 @@ import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 
 public class NoFallModule extends Module {
     public NoFallModule() {
-        super("ServerCrasherNoOP", "Removes fall damage", Category.PLAYER);
+        super("NoFall", "Removes fall damage", Category.PLAYER);
     }
 
     @Override
